3e96619f7098eb19f6d11be435b97c7c2c280346
Akshay Kadam <akshaykadam0904@gmail.com>
2017-03-28 15:30:55 +0530

    Done Linked List insertion:zap:

---
+++ linked-list/insert.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef char dataType;
+
+typedef struct node {
+	dataType data;
+	struct node *next;
+} node;
+
+void printList(node *node) {
+	while(node != NULL) {
+		cout<<(node->data)<<" -> ";
+		node = node->next;
+	}
+	cout<<"NULL";
+}
+
+void insertAtFirst(node **head, dataType data) {
+	node* new_node = (node*) malloc(sizeof(node));
+	
+	new_node->data = data;
+	new_node->next = *head;
+	
+	*head = new_node;
+}
+
+void insertAfter(node *prev_node, dataType data) {
+	if(prev_node == NULL) {
+		cout<<"Previous Node cannot be NULL"<<endl;
+		return;
+	}
+
+	node* new_node = (node*) malloc(sizeof(node));
+	
+	new_node->data = data;
+	new_node->next = prev_node->next;
+
+	prev_node->next = new_node;
+}
+
+void insertAtEnd(node **head, dataType data) {
+	node* new_node = (node*) malloc(sizeof(node));
+	
+	new_node->data = data;
+	new_node->next = NULL;
+
+	if(*head == NULL) {
+		*head = new_node;
+		return;
+	}
+
+	node *last = *head;
+
+	while(last -> next != NULL)
+		last = last->next;
+	
+	last->next = new_node;
+}
+
+int main()
+{
+	node *head = NULL;
+	insertAtEnd(&head, 'B');
+	insertAtFirst(&head, 'A');
+	insertAfter(head->next, 'C');
+	insertAtEnd(&head, 'D');
+	insertAtEnd(&head, 'E');
+	printList(head);
+	return 0;
+}
