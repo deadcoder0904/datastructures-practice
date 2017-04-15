ee8a4672f6af91180058fe385f3035338834f5c0
Akshay Kadam <akshaykadam0904@gmail.com>
2017-04-15 20:23:26 +0530

    Get Nth Node :fire:

---
+++ linked-list/get-nth-node.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef char dataType;
+
+typedef struct node {
+	dataType data;
+	struct node *next;
+} node;
+
+void printList(node *node) {
+	while(node != NULL) {
+		cout<<(node->data)<<" -> ";
+		node = node->next;
+	}
+	cout<<"NULL"<<endl;
+}
+
+void insertAtStart(node **head, dataType data) {
+	node* new_node = (node*) malloc(sizeof(node));
+	
+	new_node->data = data;
+	new_node->next = *head;
+	
+	*head = new_node;
+}
+
+dataType getNthNode(node *head, int n) {
+	node *temp = head;
+	int count = 0;
+
+	while(temp != NULL) {
+		if(count == n)
+			return temp->data;
+		count++;
+		temp = temp->next;
+	}
+
+	return '0';
+}
+
+int main()
+{
+	node *head = NULL;
+	insertAtStart(&head, 'E');
+	insertAtStart(&head, 'D');
+	insertAtStart(&head, 'C');
+	insertAtStart(&head, 'B');
+	insertAtStart(&head, 'A');
+	printList(head);
+	cout<<"Get 2nd Node : "<<getNthNode(head,1)<<endl;
+	return 0;
+}
