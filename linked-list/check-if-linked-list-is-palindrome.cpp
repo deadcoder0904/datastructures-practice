6199f42d3e09db7db77662f2b4f363cec04c94a6
Akshay Kadam <akshaykadam0904@gmail.com>
2017-04-30 14:39:06 +0530

    Check if Palindrome :apple:

--- linked-list/check-if-linked-list-is-palindrome.cpp
+++ linked-list/check-if-linked-list-is-palindrome.cpp
@@ -32,12 +32,12 @@ void insertAtStart(node **head, dataType data) {
 }
 
 node* cloneLinkedListInReverse(node *temp) {
-	node *head = NULL;
+	node *reverse = NULL;
 	while(temp != NULL) {
-		insertAtStart(&head,temp->data);
+		insertAtStart(&reverse,temp->data);
 		temp = temp->next;
 	}
-	return head;
+	return reverse;
 }
 
 int isPalindrome(node *head1,node *head2) {
